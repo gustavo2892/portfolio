18e043ff6b0a0d285d1339934a42dd163f756c5c
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-09-13 13:38:25 -0300

    Adjust Loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Redirect } from '@/components';
 export default async function RedirectPage() {
   return (
     <html>
-      <body className="relative flex min-h-screen flex-col bg-gray-700">
-        <Redirect link={'/pt'}/>
+      <body className="relative flex min-h-screen flex-col bg-secondary-100">
+        <Redirect link={'/pt'} />
       </body>
     </html>
   );
