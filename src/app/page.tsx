bd013d1ef285d7444b53e6f6cd6ac3af5fafbc89
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-12-17 15:35:29 -0300

    Remove redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { Redirect } from "@/components";
-
 export default async function RedirectPage() {
   return (
     <html>
-      <body className="relative flex min-h-screen flex-col bg-secondary-100">
-        <Redirect link={"/pt"} />
-      </body>
+      <body className="relative flex min-h-screen flex-col bg-secondary-100" />
     </html>
   );
 }
