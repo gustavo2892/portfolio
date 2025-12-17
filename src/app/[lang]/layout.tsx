fb86b30b16c7b581a3143ea648f4e31baf614dcc
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-12-17 15:12:20 -0300

    Adjust bugs

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,8 +1,8 @@
-import { PT_Sans_Caption, Montserrat } from 'next/font/google';
+import { PT_Sans_Caption, Montserrat } from "next/font/google";
 
 import { i18n, type Locale } from "@/i18n-config";
-import { Footer, Header, Whatsapp } from '@/components';
-import '@/styles/globals.css';
+import { Footer, Header, Whatsapp } from "@/components";
+import "@/styles/globals.css";
 
 export const metadata = {
   title: "Freitas Software",
@@ -15,30 +15,32 @@ export async function generateStaticParams() {
 
 type LayoutProps = {
   children: React.ReactNode;
-  params: Promise<{ lang: Locale }>
+  params: Promise<{
+    lang: string;
+  }>;
 };
 
 const ptSansCaption = PT_Sans_Caption({
-  subsets: ['latin'],
-  weight: '700',
-  variable: '--font-sans',
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-sans",
 });
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '500'],
-  variable: '--font-mont',
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-mont",
 });
 
-export default async function Root(props: LayoutProps) {
-  const params = await props.params;
-
-  const { children } = props;
+export default async function Root({ children, params }: LayoutProps) {
+  const { lang } = await params;
 
   return (
-    <html lang={params.lang}>
-      <body className={`${ptSansCaption.className} ${montserrat.className} relative flex min-h-screen flex-col bg-secondary-100`}>
-        <Header lang={params.lang} />
+    <html lang={lang}>
+      <body
+        className={`${ptSansCaption.className} ${montserrat.className} relative flex min-h-screen flex-col bg-secondary-100`}
+      >
+        <Header lang={lang} />
         <main className="flex-1 flex flex-col mb-12">{children}</main>
         <Whatsapp />
         <Footer />
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,12 @@
 import { Locale } from "@/i18n-config";
-import { Logo } from '@/components';
-import { getDictionary } from '@/get-dictionary';
-import { ActiveLink, LocaleSwitcher, MenuMobile } from './components';
+import { Logo } from "@/components";
+import { getDictionary } from "@/get-dictionary";
+import { ActiveLink, LocaleSwitcher, MenuMobile } from "./components";
 
-export const Header = async (props: {
-  lang: Locale
-}) => {
+export const Header = async (props: { lang: string }) => {
   const { lang } = props;
 
-  const dictionary = await getDictionary(lang);
+  const dictionary = await getDictionary(lang as Locale);
 
   return (
     <header className="fixed top-0 z-50 w-full bg-secondary-100 backdrop-blur supports-[backdrop-filters]:bg-background/60">
@@ -18,12 +16,14 @@ export const Header = async (props: {
 
           <nav className="hidden md:flex items-center gap-6">
             <ActiveLink href={`/${lang}`}>{dictionary.pages.home}</ActiveLink>
-            <ActiveLink href={`/${lang}/about-me`}>{dictionary.pages["about-me"]}</ActiveLink>
+            <ActiveLink href={`/${lang}/about-me`}>
+              {dictionary.pages["about-me"]}
+            </ActiveLink>
             {/*<ActiveLink href={`/${lang}/projects`}>{dictionary.pages.projects}</ActiveLink>*/}
             <LocaleSwitcher />
           </nav>
 
-          <MenuMobile dictionary={dictionary} lang={lang} />
+          <MenuMobile dictionary={dictionary} lang={lang as Locale} />
         </div>
       </div>
     </header>
