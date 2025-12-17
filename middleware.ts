c3a6c7cea91a96407743fb4d8f91106c8cafd8d5
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-12-17 15:44:10 -0300

    Adjust middleware i18

--- middleware.ts
+++ middleware.ts
@@ -6,17 +6,18 @@ import { i18n } from "./src/i18n-config";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 
+const PUBLIC_FILE = /\.(.*)$/;
+
 function getLocale(request: NextRequest): string | undefined {
-  // Negotiator expects plain object so we need to transform headers
   const negotiatorHeaders: Record<string, string> = {};
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-  // @ts-ignore locales are readonly
+  // @ts-expect-error locales are readonly
   const locales: string[] = i18n.locales;
 
   // Use negotiator and intl-localematcher to get best locale
-  let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
-    locales,
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
+    locales
   );
 
   const locale = matchLocale(languages, locales, i18n.defaultLocale);
@@ -27,21 +28,17 @@ function getLocale(request: NextRequest): string | undefined {
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    PUBLIC_FILE.test(pathname)
+  ) {
+    return;
+  }
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) =>
-      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
   // Redirect if there is no locale
@@ -53,8 +50,8 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(
       new URL(
         `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-        request.url,
-      ),
+        request.url
+      )
     );
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Redirect } from "@/components";
+
 export default async function RedirectPage() {
   return (
     <html>
-      <body className="relative flex min-h-screen flex-col bg-secondary-100" />
+      <body className="relative flex min-h-screen flex-col bg-secondary-100">
+        <Redirect link={"/pt"} />
+      </body>
     </html>
   );
 }
