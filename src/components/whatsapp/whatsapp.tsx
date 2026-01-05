430b0ead654013cd88fc41163ed62fd33f23e9d5
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2026-01-04 21:17:53 -0300

    Adjust whatsapp icon

--- src/components/whatsapp/whatsapp.tsx
+++ src/components/whatsapp/whatsapp.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Whatsapp = () => {
   return (
-    <div className="fixed bottom-18 md:bottom-10 lg:bottom-10 right-8 z-50 sm:right-8">
+    <div className="fixed bottom-8 md:bottom-10 lg:bottom-10 right-4 z-50 sm:right-8">
       <span className="relative flex size-20">
         <span className="absolute h-full w-full animate-ping rounded-full bg-emerald-400 opacity-75" />
         <span className="relative rounded-full bg-transparent">
